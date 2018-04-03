77c1954212cee96a8afbd141f0f40ab0a5d5d19c
XDean <373216024@qq.com>
2018-04-03 21:40:52 +0800

    add doc

--- src/main/java/xdean/auto/message/AutoMessage.java
+++ src/main/java/xdean/auto/message/AutoMessage.java
@@ -7,14 +7,32 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.util.Properties;
 
+/**
+ * Auto generate {@link Properties} file's keys.
+ *
+ * @author Dean Xu (XDean@github.com)
+ */
 @Documented
 @Retention(RUNTIME)
 @Target({ TYPE, PACKAGE })
 public @interface AutoMessage {
+  /**
+   * The properties path. The path has same rule with
+   * {@link Class#getResource(String)}.
+   *
+   * @see Class#getResource(String)
+   */
   String path();
 
+  /**
+   * Generated java file's name.
+   */
   String generatedName() default "Messages";
 
+  /**
+   * The properties file charset.
+   */
   String charset() default "UTF-8";
 }
